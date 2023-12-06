57217dd0ac62159513a5d8f690f0c3d0f3fb6411
miguelreched <121823965+miguelreched@users.noreply.github.com>
2023-12-06 21:02:24 +0100

    mensajes ok menos reciprocidad

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,69 +1,42 @@
-
-const router = require ("express"). Router ()
-const User = require ("../models/User.model")
-const Comment = require ("../models/Comment.model")
-const isTokenOk = require("../middlewares/auth.middleware")
-
+const router = require("express").Router();
+const User = require("../models/User.model");
+const Comment = require("../models/Comment.model");
+const isTokenOk = require("../middlewares/auth.middleware");
 
 //ruta para publicar un comentario
 
-router.post("/:userId", async (req,res,next)=>{
+router.post("/:userId", async (req, res, next) => {
+  const { commenter, user, comment } = req.body;
 
-    const {commenter, user, comment} = req.body
+  try {
+    await Comment.create({
+      commenter, //utilizar el payload
+      user,
+      comment,
+    });
 
-    try {
-
-
-        await Comment.create ({
-    
-            commenter,//utilizar el payload
-            user,
-            comment
-    
-    
-        })
-    
-        res.status(201).json ("Comment Created")
-
-
-
-
-    }catch (error){
-
-        next (error)
-    }
-
-
-
-})
-
-
-
+    res.status(201).json("Comment Created");
+  } catch (error) {
+    next(error);
+  }
+});
 
 // ruta para ver los comentarios en el user profile
-router.get("/:userId", async (req,res,next)=>{
+router.get("/:userId", async (req, res, next) => {
+  console.log(req.params.userId, "ACAAAAAAAA");
 
-    console.log(req.params.userId, "AQUIIIIII")
+  try {
+    const comment = await Comment.find({ user: req.params.userId }).populate(
+      "commenter",
+      "username"
+    );
 
-    try {
+    console.log(comment);
 
-        const comment = await Comment.find({ user:req.params.userId });
-        console.log(comment)
-
-        res.json (comment) 
-
-
-
-    }catch(error){
-
-        next(error)
-    }
-
-
-
-})
-
-
-
+    res.json(comment);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- routes/messages.routes.js
+++ routes/messages.routes.js
@@ -1,69 +1,48 @@
-const router = require ("express"). Router ()
-const Message = require ("../models/Message.model")
-const User = require ("../models/User.model")
-const isTokenOk = require("../middlewares/auth.middleware")
-
+const router = require("express").Router();
+const Message = require("../models/Message.model");
+const User = require("../models/User.model");
+const isTokenOk = require("../middlewares/auth.middleware");
 
 //ruta para  publicar un mensaje
 
+router.post("/:userId", async (req, res, next) => {
+  const { sender, receiver, message } = req.body;
 
-router.post ("/:userId", async (req,res,next) =>{
+  try {
+    await Message.create({
+      sender,
+      receiver,
+      message,
+    });
 
-    const {sender, receiver, message} = req.body
-
-
-        try{
-
-            await Message.create ({
-
-                sender,
-                receiver,
-                message
-
-
-            })
-
-            res.status(201).json ("Message Created")
-
-        }catch(error){
-
-            next(error)
-
-        }
-
-
-
-
-})
-
+    res.status(201).json("Message Created");
+  } catch (error) {
+    next(error);
+  }
+});
 
 // ruta para ver los mensajes
-router.get("/:userId", async (req,res,next)=>{
+router.get("/:userId", async (req, res, next) => {
+    console.log(req.params.userId, "AQUIIIIII");
+  try {
+    const message = await Message.find({ receiver: req.params.userId }).populate(
+      "sender",
+      "username"
+    );
 
-    
+    const messageBack = await Message.find({ sender: req.params.userId }).populate(
+        "receiver",
+        "username"
+      );
 
-    try {
+      const allMessages = [...message, ...messageBack]
+      .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
 
-        const message = await Message.find({ userId: req.body._id });
-
-        // const message = await User.findById(userId).populate(req.body.username);  
-        
-        // const message = await Message.findById(userId).populate(sender.username);  
-
-        res.json (message) 
-
-
-
-    }catch(error){
-
-        next(error)
-    }
-
-
-
-})
-
-
-
+    console.log(message);
+    res.json(allMessages);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
